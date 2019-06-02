36be31885be26a54a332340d1762f4df33413115
dimchel <d.kidakov@space307.com>
2019-06-02 11:32:22 +0300

    added lottie animation

--- app/src/main/java/com/dimchel/annatest/features/animation/AnimationFragment.kt
+++ app/src/main/java/com/dimchel/annatest/features/animation/AnimationFragment.kt
@@ -1,7 +1,16 @@
 package com.dimchel.annatest.features.animation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.dimchel.annatest.R
 
 class AnimationFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_animation, container, false)
 }
